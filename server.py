0caee4cbcccf09f50ec83b64e6cc4471396f4b1a
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2023-02-02 21:46:13 +0600

    Update server.py
    added authorization and made stopped by default

--- server.py
+++ server.py
@@ -1,52 +1,72 @@
 import openai
 import telegram
 from telegram import Update
-from telegram.ext import Updater, MessageHandler, CommandHandler, Filters
+from telegram.ext import Updater, MessageHandler, CommandHandler, CallbackContext, Filters
 
-def start(update: Update, context):
-    context.user_data['ignored'] = False
-    update.message.reply_text("Started listening to messages")
+# Replace with the API key for your OpenAI account
+openai.api_key = "YOUR_OPENAI_API_KEY"
 
-def stop(update: Update, context):
-    context.user_data['ignored'] = True
-    update.message.reply_text("Stopped listening to messages")
+# Replace with the bot token you received from BotFather
+updater = Updater(token='BOT_TOKEN', use_context=True)
+dispatcher = updater.dispatcher
 
-def clear(update: Update, context):
-    context.user_data.clear()
-    update.message.reply_text("History cleared")
+# Authorize user
+def auth(update: Update, context: CallbackContext):
+    if update.message.text == "/auth YOUR_SECRET_PASSWORD":
+        context.user_data['authorized'] = True
+        update.message.reply_text("Authorization successful.")
+    else:
+        update.message.reply_text("Authorization failed.")
 
-def respond(update: Update, context):
-    if 'ignored' in context.user_data and context.user_data['ignored']:
+# Start handling messages
+def start(update: Update, context: CallbackContext):
+    context.user_data['ignore'] = False
+    update.message.reply_text("Started handling messages.")
+
+# Stop handling messages
+def stop(update: Update, context: CallbackContext):
+    context.user_data['ignore'] = True
+    update.message.reply_text("Stopped handling messages.")
+
+# Clear history of messages
+def clear(update: Update, context: CallbackContext):
+    context.user_data['history'] = []
+    update.message.reply_text("History cleared.")
+
+# Handle messages
+def handle_message(update: Update, context: CallbackContext):
+    if not context.user_data.get('authorized', False):
+        update.message.reply_text("You are not authorized.")
         return
 
-    openai.api_key = "YOUR_OPENAI_API_KEY"
-    model_engine = "text-davinci-003"
-    prompt = f"{' '.join(context.user_data.get('history', []))} {update.message.text}"
+    if context.user_data.get('ignore', True):
+        return
+
+    context.user_data['history'] = context.user_data.get('history', []) + [update.message.text]
+    prompt = ' '.join(context.user_data['history'])
 
     response = openai.Completion.create(
-        engine=model_engine,
+        engine="text-davinci-002",
         prompt=prompt,
-        max_tokens=2048,
+        max_tokens=1024,
         n=1,
         stop=None,
         temperature=0.5,
     ).choices[0].text
 
-    context.user_data['history'] = context.user_data.get('history', []) + [update.message.text, response]
+    context.user_data['history'] = context.user_data.get('history', []) + [response]
     update.message.reply_text(response)
 
-def main():
-    openai.api_key = "YOUR_OPENAI_API_KEY"
-    updater = Updater("YOUR_TELEGRAM_BOTFATHER_TOKEN", use_context=True)
+auth_handler = CommandHandler('auth', auth)
+start_handler = CommandHandler('start', start)
+stop_handler = CommandHandler('stop', stop)
+clear_handler = CommandHandler('clear', clear)
+message_handler = MessageHandler(Filters.text & (~Filters.command), handle_message)
 
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("stop", stop))
-    dp.add_handler(CommandHandler("clear", clear))
-    dp.add_handler(MessageHandler(Filters.text, respond))
+dispatcher.add_handler(auth_handler)
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(stop_handler)
+dispatcher.add_handler(clear_handler)
+dispatcher.add_handler(message_handler)
 
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+updater.start_polling()
