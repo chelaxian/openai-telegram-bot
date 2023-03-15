ea6c5e1ed385b688a405b81cdf071b5a411a99ae
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2023-03-15 13:00:17 +0300

    Update server.py
    add /ask command to answer only messegess starting with /ask
    (now you can add it to group chat and don't worry about bot answering all messeges)

--- server.py
+++ server.py
@@ -14,41 +14,48 @@ dispatcher = updater.dispatcher
 def auth(update: Update, context: CallbackContext):
     if update.message.text == "/auth YOUR_SECRET_PASSWORD":
         context.user_data['authorized'] = True
-        update.message.reply_text("Authorization successful.")
+        update.message.reply_text("Authorization passed successfully!")
     else:
-        update.message.reply_text("Authorization failed.")
+        update.message.reply_text("Incorrect password!")
 
 # Start handling messages
 def start(update: Update, context: CallbackContext):
-    context.user_data['ignore'] = False
-    update.message.reply_text("Started handling messages.")
+    if context.user_data['authorized'] == True:
+        context.user_data['ignore'] = False
+        update.message.reply_text("I start listening to messages and replying to them.")
+    else: 
+        update.message.reply_text("
+First you need to pass authorization!")
 
 # Stop handling messages
 def stop(update: Update, context: CallbackContext):
-    context.user_data['ignore'] = True
-    update.message.reply_text("Stopped handling messages.")
+    if context.user_data['authorized'] == True:
+        context.user_data['ignore'] = True
+        update.message.reply_text("I no longer listen to messages and do not answer them.")
+    else: 
+        update.message.reply_text("First you need to pass authorization!")
 
 # Clear history of messages
 def clear(update: Update, context: CallbackContext):
-    context.user_data['history'] = []
-    update.message.reply_text("History cleared.")
+    if context.user_data['authorized'] == True:
+        context.user_data['history'] = []
+        update.message.reply_text("The message history has been reset. The context of the conversation is forgotten.")
+    else: 
+        update.message.reply_text("First you need to pass authorization!")
 
 # Handle messages
-def handle_message(update: Update, context: CallbackContext):
+def ask(update: Update, context: CallbackContext):
     if not context.user_data.get('authorized', False):
-        update.message.reply_text("You are not authorized.")
+        update.message.reply_text("First you need to pass authorization!")
         return
 
-    if context.user_data.get('ignore', True):
-        return
-
-    context.user_data['history'] = context.user_data.get('history', []) + [update.message.text]
+    context.user_data['history'] = context.user_data.get('history', []) + [update.message.text.replace('/ask ', '')]
     prompt = ' '.join(context.user_data['history'])
 
     response = openai.Completion.create(
-        engine="text-davinci-002",
+        engine="text-davinci-003",
         prompt=prompt,
-        max_tokens=1024,
+        max_tokens=2048,
         n=1,
         stop=None,
         temperature=0.5,
@@ -61,12 +68,14 @@ auth_handler = CommandHandler('auth', auth)
 start_handler = CommandHandler('start', start)
 stop_handler = CommandHandler('stop', stop)
 clear_handler = CommandHandler('clear', clear)
-message_handler = MessageHandler(Filters.text & (~Filters.command), handle_message)
+ask_handler = CommandHandler('ask', ask)
+
 
 dispatcher.add_handler(auth_handler)
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(stop_handler)
 dispatcher.add_handler(clear_handler)
-dispatcher.add_handler(message_handler)
+dispatcher.add_handler(ask_handler)
+
 
 updater.start_polling()
